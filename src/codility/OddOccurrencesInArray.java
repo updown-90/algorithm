a0466a5dbba1e2a995638fb2669c528cc258ec48
updown-90 <updown_90@kakao.com>
2021-10-20 22:54:45 +0900

    https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/

--- src/codility/OddOccurrencesInArray.java
+++ src/codility/OddOccurrencesInArray.java
@@ -1,18 +1,22 @@
 package codility;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class OddOccurrencesInArray {
     public static void main(String[] args) {
-        int X = 10;
-        int Y = 85;
-        int D = 30;
-
-        int returnValue = 0;
-        while (X < Y) {
-            X = X + D;
-            returnValue++;
+        int[] A = {9, 3, 9, 3, 9, 7, 9};
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : A) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
 
-        System.out.println(returnValue);
+        int returnValue;
+        for (Integer integer : map.keySet()) {
+            if(map.get(integer) == 1){
+                returnValue = integer;
+            }
+        }
     }
 
 
