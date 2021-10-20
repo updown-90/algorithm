268836827cfdf519441cdab56a5803e2a247bf46
updown-90 <updown_90@kakao.com>
2021-10-20 22:55:37 +0900

    https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/

---
+++ src/codility/FrogJmp.java
@@ -0,0 +1,17 @@
+package codility;
+
+public class FrogJmp {
+    public static void main(String[] args) {
+        int X = 10;
+        int Y = 85;
+        int D = 30;
+
+        int returnValue = 0;
+        while (X < Y) {
+            X = X + D;
+            returnValue++;
+        }
+
+        System.out.println(returnValue);
+    }
+}
