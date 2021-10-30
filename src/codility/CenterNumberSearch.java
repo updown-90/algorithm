6f4fb5f28bf67b4552e32578341c5fe5654df776
updown-90 <updown_90@kakao.com>
2021-10-31 05:42:33 +0900

    add: 배열에서 가운데 수 구하기

---
+++ src/codility/CenterNumberSearch.java
@@ -0,0 +1,21 @@
+package codility;
+
+import java.util.Arrays;
+
+public class CenterNumberSearch {
+    public static void main(String[] args) {
+        int[] A = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        Arrays.sort(A);
+
+        if (A.length % 2 == 0) {
+            int a = A.length / 2;
+            float b = a + 1;
+            System.out.println((a + b) / 2);
+        } else {
+            int a = A.length / 2;
+            System.out.println(A[a]);
+        }
+
+    }
+}
