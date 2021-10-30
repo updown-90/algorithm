5d24d5b9826d459fa5c1bbeffd16017698fa9e21
updown-90 <updown_90@kakao.com>
2021-10-31 06:15:35 +0900

    add: 배열뒤집기

--- src/Reverse.java
+++ src/Reverse.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
-public class GausSum {
+public class Reverse {
 
     public static void main(String[] args) {
         int[] A = {1, 2, 3, 4, 5, 6, 7, 8, 9};
