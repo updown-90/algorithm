be06684701648df205e5895372f047ee46a9c5ca
updown-90 <52308702+updown-90@users.noreply.github.com>
2022-12-11 20:21:52 +0900

    feat: 백준 2920번 풀이

---
+++ src/baekjoon/Problem2920.java
@@ -0,0 +1,37 @@
+package baekjoon;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Problem2920 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine();
+        String[] sArr = inputString.split(" ");
+
+
+        boolean asc = true;
+        boolean desc = true;
+        for (int i = 1; i < sArr.length; i++) {
+// 1,2,3
+            // 내림차순이면
+            if (Integer.parseInt(sArr[i - 1]) > Integer.parseInt(sArr[i])) {
+                asc = false;
+            }
+            // 오름차순이면
+            else if (Integer.parseInt(sArr[i - 1]) < Integer.parseInt(sArr[i])) {
+                desc = false;
+            }
+
+        }
+
+        if (asc) {
+            System.out.println("ascending");
+        } else if (desc) {
+            System.out.println("descending");
+        } else {
+            System.out.println("mixed");
+        }
+    }
+}
